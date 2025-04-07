aec45f11012044949e2d923367f45363cdfe560e
brights0ng <sdcolson1@gmail.com>
2025-04-07 00:02:59 -0600

    Added Phantom-6
    Technically just a crossbow remodeled, and it belongs in the kinetic armory mod, not starflight, but thats next month brights0ng's problems

--- src/main/java/net/starflight/item/ModItems.java
+++ src/main/java/net/starflight/item/ModItems.java
@@ -15,6 +15,7 @@ public class ModItems {
     public static final Item ARMOR_PANEL_ITEM = new BlockItem(ModBlocks.ARMOR_PANEL, new Item.Settings());
     public static final Item MAG_PAD_ITEM = new BlockItem(ModBlocks.MAG_PAD, new Item.Settings());
     public static final Item HYDROGEN_THRUSTER_ITEM = new BlockItem(ModBlocks.HYDROGEN_THRUSTER, new Item.Settings());
+    public static final Phantom6Item PHANTOM_6 = new Phantom6Item(new Item.Settings());
 
     public static final ItemGroup STARFLIGHT_BLOCKS = new ItemGroup.Builder(ItemGroup.Row.TOP, 1)
             .displayName(Text.literal("Stardance Utils"))
@@ -23,6 +24,8 @@ public class ModItems {
                 entries.add(MAG_PAD_ITEM);
                 entries.add(ARMOR_PANEL_ITEM);
                 entries.add(HYDROGEN_THRUSTER_ITEM);
+                entries.add(PHANTOM_6);
+
             })
             .build();
 
@@ -30,6 +33,7 @@ public class ModItems {
         Registry.register(Registries.ITEM, new Identifier(Starflight.MOD_ID, "armor_panel"), ARMOR_PANEL_ITEM);
         Registry.register(Registries.ITEM, new Identifier(Starflight.MOD_ID, "mag_pad"), MAG_PAD_ITEM);
         Registry.register(Registries.ITEM, new Identifier(Starflight.MOD_ID, "hydrogen_thruster"), HYDROGEN_THRUSTER_ITEM);
+        Registry.register(Registries.ITEM, new Identifier(Starflight.MOD_ID, "phantom-6"), PHANTOM_6);
 
         Registry.register(Registries.ITEM_GROUP, new Identifier(Starflight.MOD_ID, "starflight_blocks"), STARFLIGHT_BLOCKS);
     }
---
+++ src/main/java/net/starflight/item/Phantom6Item.java
@@ -0,0 +1,12 @@
+package net.starflight.item;
+
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class Phantom6Item extends CrossbowItem {
+
+    public Phantom6Item(Settings settings) {
+        super(settings);
+    }
+}
